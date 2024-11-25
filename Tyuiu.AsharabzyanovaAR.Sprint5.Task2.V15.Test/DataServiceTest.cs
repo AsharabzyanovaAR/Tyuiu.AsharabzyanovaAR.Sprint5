55c5d55130304792345415cbef3ac110ae1131ac
AsharabzyanovaAR <rustamovna14082006@gmail.com>
2024-11-25 20:51:10 +0500

    Task2

---
+++ Tyuiu.AsharabzyanovaAR.Sprint5.Task2.V15.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Tyuiu.AsharabzyanovaAR.Sprint5.Task2.V15.Lib;
+namespace Tyuiu.AsharabzyanovaAR.Sprint5.Task2.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\HONOR\AppData\Local\Temp\tmpvui2h0.tmp";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.AsharabzyanovaAR.Sprint5.Task2.V15/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.AsharabzyanovaAR.Sprint5.Task2.V15.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #5 | Выполнила: Ашарабзянова А.Р. | ПКТб-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #15                                                             *");
+        Console.WriteLine("* Выполнила: Ашарабзянова Алина Рустамовна | ПКТб-24-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+        Console.WriteLine("* значениями с клавиатуры. Заменить нечетные элементы массива на 0.       *");
+        Console.WriteLine("* Результат сохранить в файл OutPutFileTask2.csv и вывести на консоль.    *");
+        Console.WriteLine("***************************************************************************");
+        int[,] matrix =new int[3, 3]{{6, 1, 7 },
+                                     {1, 8, 5 },
+                                     {6, 6, 4 }};
+        int rows = matrix.GetUpperBound(0)+1;
+        int columns = matrix.Length/rows;
+
+        DataService dataService = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{matrix[i, j]}\t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        string res = dataService.SaveToFileTextData(matrix);
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+    }
+}
