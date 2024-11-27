3cc767928622c5cc415b69a674496398f9600847
AsharabzyanovaAR <rustamovna14082006@gmail.com>
2024-11-27 10:53:40 +0500

    Task4

--- Tyuiu.AsharabzyanovaAR.Sprint5.Task4.V10.Lib/DataService.cs
+++ Tyuiu.AsharabzyanovaAR.Sprint5.Task4.V10.Lib/DataService.cs
@@ -7,6 +7,7 @@ namespace Tyuiu.AsharabzyanovaAR.Sprint5.Task4.V10.Lib
         public double LoadFromDataFile(string path)
         {
             string strx = File.ReadAllText(path);
+            strx = strx.Replace('.', ',');
             double res = Math.Round(Math.Pow(Convert.ToDouble(strx), 3) * 1.2 * Convert.ToDouble(strx) + 2, 3);
             return res;
         }
