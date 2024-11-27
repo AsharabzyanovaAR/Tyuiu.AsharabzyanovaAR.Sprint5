7c71479d10b34ef2a986e72036615a87382e26ec
AsharabzyanovaAR <rustamovna14082006@gmail.com>
2024-11-27 12:29:26 +0500

    Task5

--- Tyuiu.AsharabzyanovaAR.Sprint5.Task5.V6.Lib/DataService.cs
+++ Tyuiu.AsharabzyanovaAR.Sprint5.Task5.V6.Lib/DataService.cs
@@ -1,5 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint5;
 using System.IO;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 namespace Tyuiu.AsharabzyanovaAR.Sprint5.Task5.V6.Lib
 {
     public class DataService : ISprint5Task5V6
@@ -7,19 +9,22 @@ namespace Tyuiu.AsharabzyanovaAR.Sprint5.Task5.V6.Lib
         public double LoadFromDataFile(string path)
         {
             double res = 0;
-            int k = 0;
-            
-            using (StreamReader reader = new StreamReader(path))
+            double k = 0;
+            using (StreamReader sr = new StreamReader(path))
             {
-                string line;
-                while ((line = reader.ReadLine() ) != null)
+                string? line;
+                while ((line = sr.ReadLine()) != null)
                 {
-                    line = line.Replace('.', ',');
-                    k += 1;
-                    res = res + Math.Round(Convert.ToDouble(line),3);
+                    string[] numbers = line.Split(' ');
+                    foreach (string number in numbers)
+                    {
+                        double number_ = double.Parse(number, CultureInfo.InvariantCulture);
+                        k += 1;
+                        res = res + Math.Round(number_, 3);
+                    }
                 }
             }
-            return (res/k);
+            return res/k;
         }
     }
 }
